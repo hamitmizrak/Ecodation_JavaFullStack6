8c2912104faceefcb019a976af12cc364e227420
hamitmizrak <hamitmizrak@gmail.com>
2022-11-12 15:50:56 +0300

    JS -1

---
+++ frontend/_04_javascript/Template.js
@@ -0,0 +1,70 @@
+//alert("merhabalar")
+
+//console
+//console.log("console log")
+
+//alert
+//alert("deneme1")
+//window.alert("alert1")
+
+//
+//document.write("document data")
+
+// double quotes: çift tırnak ("")
+// single quotes: tek tırnak ('')
+// backtick : tek tırnak (``)
+
+//var (Global)
+//let(Local) ES6
+//const(Local) ES6 
+// var _$degiskenAdi88=44;
+// console.log(_$degiskenAdi88)
+// /*
+// Değişken adları: kullanma
+// sayıyla başlama
+// _ve $ haricinde özel bir simgeyi kabul etmiyor
+// kelimeler arasında tire kullanma
+// */
+
+// var value1=44;
+// console.log(typeof value1)
+
+// var value2=44.5656;
+// console.log(typeof value2)
+
+// var value3="JavaScript öğreniyorum";
+// console.log(typeof value3)
+
+// var value4=false;
+// console.log(typeof value4)
+
+// var value5=[]; //dizi
+// console.log(typeof value5)
+
+// var value6={}; //object
+// console.log(typeof value6)
+
+// //undefined
+// let data;
+// console.log(data)
+
+//+ - * / % x++ y-- ++X
+let x1=10;
+const x2=3;
+
+var x3=x1+x2;
+console.log(x3);
+
+var x4=x1-x2;
+console.log(x4);
+
+var x5=x1*x2;
+console.log(x5);
+
+var x6=x1/x2;
+console.log(x6);
+
+var x7=x1%x2;
+console.log(x7);
+
+//
