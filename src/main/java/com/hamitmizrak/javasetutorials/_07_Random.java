d11f9ac61225b3e186efe0172b97ff9283fcb9d8
hamitmizrak <hamitmizrak@gmail.com>
2022-12-10 18:23:59 +0300

    Random

---
+++ src/main/java/com/hamitmizrak/javasetutorials/_07_Random.java
@@ -0,0 +1,17 @@
+package com.hamitmizrak.javasetutorials;
+
+import java.util.Random;
+
+public class _07_Random {
+    public static void main(String[] args) {
+       //Math
+        System.out.println( Math.round(Math.random()*4+1));
+
+        //Random :Object
+        Random random=new Random();
+        int number=random.nextInt(4)+1;
+        System.out.println(number);
+
+
+    }
+}
