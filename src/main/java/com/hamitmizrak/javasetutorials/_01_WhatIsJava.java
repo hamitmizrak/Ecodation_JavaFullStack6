520980ae8a0e72cd24f5a99e5b36e9fce982b6ac
hamitmizrak <hamitmizrak@gmail.com>
2022-12-10 15:35:00 +0300

    javase

---
+++ src/main/java/com/hamitmizrak/javasetutorials/_01_WhatIsJava.java
@@ -0,0 +1,32 @@
+package com.hamitmizrak.javasetutorials;
+
+/*
+Yüksek Seviyeli bir programlama dilidir.
+OAK
+*/
+public class _01_WhatIsJava {
+
+    //psvm ==> TAB
+    public static void main(String[] args) {
+        //String _$degiskenAdi41;
+        //String -+~44degiskenAdi;
+        String üğşçödegiskenAdi;
+
+        //operatörler
+        int number1=20;
+        int number2=10;
+        //sout ==> TAB
+        System.out.println(number1+number2);
+        System.out.println(number1-number2);
+        System.out.println(number1*number2);
+        System.out.println(number1/number2);
+        System.out.println(number1%number2);
+
+        // < <= > >= ! ++ --
+
+        //primitive type
+
+        //wrapper type
+
+    }
+}
