9978c50581c66b27539fdaf964be124bebcb91cb
hamitmizrak <hamitmizrak@gmail.com>
2022-12-10 18:22:10 +0300

    Math

---
+++ src/main/java/com/hamitmizrak/javasetutorials/_06_Math.java
@@ -0,0 +1,22 @@
+package com.hamitmizrak.javasetutorials;
+
+public class _06_Math {
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+
+        System.out.println(Math.min(1,-900));
+        System.out.println(Math.max(1,-900));
+        System.out.println(Math.abs(-45));
+        System.out.println(Math.pow(2,5));
+
+        System.out.println(Math.floor(4.9));
+        System.out.println(Math.ceil(4.1));
+        System.out.println(Math.round(9.4));
+        System.out.println(Math.round(9.5));
+
+        System.out.println( Math.round(Math.random()*4+1));
+
+
+    }
+}
