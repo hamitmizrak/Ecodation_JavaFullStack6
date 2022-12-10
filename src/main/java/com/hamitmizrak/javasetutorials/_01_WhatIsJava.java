95f8aecd28e85a7eb83b2a4e0152195b75a43af9
hamitmizrak <hamitmizrak@gmail.com>
2022-12-10 15:48:43 +0300

    primitive

--- src/main/java/com/hamitmizrak/javasetutorials/_01_WhatIsJava.java
+++ src/main/java/com/hamitmizrak/javasetutorials/_01_WhatIsJava.java
@@ -16,15 +16,33 @@ public class _01_WhatIsJava {
         int number1=20;
         int number2=10;
         //sout ==> TAB
-        System.out.println(number1+number2);
+        /*System.out.println(number1+number2);
         System.out.println(number1-number2);
         System.out.println(number1*number2);
         System.out.println(number1/number2);
         System.out.println(number1%number2);
-
+        */
         // < <= > >= ! ++ --
 
-        //primitive type
+        // primitive type
+        // stack memory:
+        // 8 tane primitive type vardır
+        // tamsayılar b s i l
+        // -128 <=X<127 2^N
+        byte b1=44;
+        short s1=130;
+        int i1=1551511111;
+        long l1=15515111111L;
+
+        // virgüllü sayı
+        float f2=1515.45f;
+        double d2=15511515.4545445;
+
+        // boolean
+        boolean b3=false;
+
+        // karakter
+        char c4='@';
 
         //wrapper type
 
