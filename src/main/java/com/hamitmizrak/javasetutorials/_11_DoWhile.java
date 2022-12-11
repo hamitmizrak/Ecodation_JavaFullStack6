61dc31f54b57710a470747a8372f55bc76707787
hamitmizrak <hamitmizrak@gmail.com>
2022-12-11 15:33:58 +0300

    Loop

---
+++ src/main/java/com/hamitmizrak/javasetutorials/_11_DoWhile.java
@@ -0,0 +1,35 @@
+package com.hamitmizrak.javasetutorials;
+
+public class _11_DoWhile {
+
+    public static void main(String[] args) {
+        // Conditional
+        // Loop
+        // Date
+        // Method
+        // Try-catch
+
+        System.out.println("For");
+        //For
+        for (int i = 1; i <=10 ; i++) {
+            System.out.print(i+" ");
+        }
+
+        System.out.println("\nWHILE");
+
+        int k = 1;
+        //while
+        while(k <=10 ){
+            System.out.print(k+" ");
+            k++;
+        }
+
+        System.out.println("\nDO-WHILE");
+        int m = 1;  ;
+        do{
+            System.out.print(m+" ");
+            m++;
+        }while(m <=10);
+
+    }
+}
