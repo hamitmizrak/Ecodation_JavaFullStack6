018834f6d1029d3625c05f976582fc62a8a593bc
hamitmizrak <hamitmizrak@gmail.com>
2022-12-11 15:36:35 +0300

    Examples

---
+++ src/main/java/com/hamitmizrak/javasetutorials/_12_Examples.java
@@ -0,0 +1,27 @@
+package com.hamitmizrak.javasetutorials;
+
+public class _12_Examples {
+    public static void main(String[] args) {
+        // Kullanıcı tarafından girilen haftanın gününü bulan algoritma Scanner,Swich ?
+        // Rastgele 5 tane sayının toplamı , çift sayı adedi,çift sayı toplamı ?
+
+
+
+        // Vize Final Ödevi
+        // kullanıcıdan alınan vize ve final notuna göre geçme(ortalama) Algoritması
+        // eğer kullanıcı vize veya final 0(sıfır) bir değer girerse sistemden atsın
+        // ekranda "Sifir disiplin hatasi Sistemden cikiliyor"
+        // bunun haricinde sürekli vize final sorsun
+        // Not Harfleri ==> AA BA BB FF
+        // not ortalaması: ortalama<50 altında ise kaldı FF
+        //not ortalaması: ortalama==50 Geçti
+        // not ortalaması: 55<=x<=69 BB
+        // not ortalaması: 70<=x<=84 BA
+        // not ortalaması: 85<=x<=100 AA
+        // Vize:40% Final:60%
+        // Dikkat: Consolda virgül kullanmalısın.
+        // while sonsuz döngü   ==> while(true) {}
+        // for sonsuz döngü     ==> for(;;) {}
+
+    }
+}
