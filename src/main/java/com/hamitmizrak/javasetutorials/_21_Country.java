6991090a98c52cf5e0b86e4727568cbea9afe84e
hamitmizrak <hamitmizrak@gmail.com>
2022-12-18 15:25:01 +0300

    Inner Class

---
+++ src/main/java/com/hamitmizrak/javasetutorials/_21_Country.java
@@ -0,0 +1,34 @@
+package com.hamitmizrak.javasetutorials;
+
+import lombok.Getter;
+import lombok.Setter;
+
+//Inner Class: iç içe java classları
+// java 1 tane public class vardır(inner class Hariç)
+// javada classlar static olamaz (inner class Hariç)
+public class _21_Country {
+
+    //field
+    @Getter
+    @Setter
+    private String countryName;
+
+    //Inner class City
+    public static class City {
+        @Getter
+        @Setter
+        private String cityName;
+    }
+
+}
+
+class MainClass{
+    public static void main(String[] args) {
+        _21_Country country=new _21_Country();
+        country.setCountryName("Türkiye");
+
+        _21_Country.City city=new _21_Country.City();
+        city.setCityName("Ankara");
+        System.out.println(country.getCountryName()+" "+city.getCityName());
+    }
+}
