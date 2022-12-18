accbba49ea3b1fe55ea8ee5d3e1a80b343ad695e
hamitmizrak <hamitmizrak@gmail.com>
2022-12-18 18:50:27 +0300

    FileIO Login

--- src/main/java/com/hamitmizrak/fileio/Login.java
+++ src/main/java/com/hamitmizrak/fileio/Login.java
@@ -1,7 +1,18 @@
 package com.hamitmizrak.fileio;
 
+import lombok.*;
+import lombok.extern.log4j.Log4j2;
+
 import java.util.Scanner;
 
+//lombok
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Log4j2
 public class Login {
     //field
     private String username;
@@ -9,109 +20,74 @@ public class Login {
 
     //static: durağan:
     //Kullancınının yapacağı 3 kere yanlış şifre
-    private static int COUNTER=2;
+    private static int COUNTER = 2;
 
-    //parametresiz constructor
-    public Login() {
-    }
-
-    //parametreli constructor
-    public Login(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    //toString
-    @Override
-    public String toString() {
-        return "LoginDto{" +
-                "username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
     //metotlar
    /*
     admin : username(admin)  ,password(passwd)
     writer: username(writer) ,password(passwd)
     user  : username(user)   ,password(passwd)
     */
-    public Login[] allPersonLoginData(){
-        Login[] person=new Login[3];
-        person[0]=new Login(ERoles.ADMIN.getValue(),"123");
-        person[1]=new Login(ERoles.WRITER.getValue(),"123");
-        person[2]=new Login(ERoles.USER.getValue(),"123");
+    public Login[] allPersonLoginData() {
+        Login[] person = new Login[3];
+        person[0] = new Login(ERoles.ADMIN.getValue(), "123");
+        person[1] = new Login(ERoles.WRITER.getValue(), "123");
+        person[2] = new Login(ERoles.USER.getValue(), "123");
         return person;
     }
 
     //username
-    public String[] getUserInput(){
-        Scanner klavye=new Scanner(System.in);
-        String[] login=new String[2];
-        String username,password;
+    public String[] getUserInput() {
+        Scanner klavye = new Scanner(System.in);
+        String[] login = new String[2];
+        String username, password;
         System.out.println("\nLütfen kullanıcı adınızı giriniz");
-        username=klavye.nextLine();
+        username = klavye.nextLine();
         System.out.println("Lütfen kullanıcı şifrenizi giriniz");
-        password=klavye.nextLine();
-        login[0]=username;
-        login[1]=password;
+        password = klavye.nextLine();
+        login[0] = username;
+        login[1] = password;
         return login;
     }
 
     //system isLogin
-    public String isLogin(){
+    public String isLogin() {
         /*for ( String temp: getUserInput()) {
             System.out.println(temp);
         }*/
-        String [] userDataInput=getUserInput();
-        Login[] user= allPersonLoginData();
-        String rolles="";
+        String[] userDataInput = getUserInput();
+        Login[] user = allPersonLoginData();
+        String rolles = "";
 
-        if(COUNTER>=0){
-        if( (userDataInput[0].equals(user[0].getUsername())) && (userDataInput[1].equals(user[0].getPassword()))){
-            System.out.println("Admin");
-            rolles="Admin";
-        }else if( (userDataInput[0].equals(user[1].getUsername())) && (userDataInput[1].equals(user[1].getPassword())) ){
-            System.out.println("Writer");
-            rolles="Writer";
-        }else if( (userDataInput[0].equals(user[2].getUsername())) && (userDataInput[1].equals(user[2].getPassword())) ){
-            System.out.println("User");
-            rolles="User";
-        }else{
-            System.out.println("Kullanıcı username veya password yanlış girildi");
-            if(COUNTER==0){
-                System.err.println("Hakkınız kalmadı Hesabımız bloke oldu");
-                throw new HamitMizrakException("Dikkat: Hakkınız kalmadı ");
-                //System.exit(0);
+        if (COUNTER >= 0) {
+            if ((userDataInput[0].equals(user[0].getUsername())) && (userDataInput[1].equals(user[0].getPassword()))) {
+                System.out.println("Admin");
+                rolles = "Admin";
+            } else if ((userDataInput[0].equals(user[1].getUsername())) && (userDataInput[1].equals(user[1].getPassword()))) {
+                System.out.println("Writer");
+                rolles = "Writer";
+            } else if ((userDataInput[0].equals(user[2].getUsername())) && (userDataInput[1].equals(user[2].getPassword()))) {
+                System.out.println("User");
+                rolles = "User";
+            } else {
+                System.out.println("Kullanıcı username veya password yanlış girildi");
+                if (COUNTER == 0) {
+                    System.err.println("Hakkınız kalmadı Hesabımız bloke oldu");
+                    throw new HamitMizrakException("Dikkat: Hakkınız kalmadı ");
+                    //System.exit(0);
+                }
+                COUNTER--;
+                System.out.println("kalan hakkınız: " + (COUNTER + 1));
+                isLogin();
             }
-            COUNTER--;
-            System.out.println("kalan hakkınız: "+(COUNTER+1));
-            isLogin();
-        }
         }
         return rolles;
-    }
-
-    //Getter and setter
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     //psvm
     public static void main(String[] args) {
         //instance
-        Login login=new Login();
+        Login login = new Login();
         login.isLogin();
     }
 }
