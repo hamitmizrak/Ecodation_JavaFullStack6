c1dcce234b2321780c19023697f3123ab26ae9be
hamitmizrak <hamitmizrak@gmail.com>
2023-01-08 18:10:45 +0300

    ModelMapper, ProductController,HTML pages,ProductController

--- src/main/java/com/hamitmizrak/springboot/controller/mvc/IProduct.java
+++ src/main/java/com/hamitmizrak/springboot/controller/mvc/IProduct.java
@@ -1,6 +1,13 @@
 package com.hamitmizrak.springboot.controller.mvc;
 import com.hamitmizrak.springboot.business.dto.ProductDto;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 public interface IProduct {
 
@@ -8,5 +15,19 @@ public interface IProduct {
     //CREATE
     //http://localhost:8080/product/create
     String  createGet(Model model);
-    String  createPost(ProductDto productDto, Model model);
+
+    //http://localhost:8080/product/create
+    String  createPost(ProductDto productDto, BindingResult bindingResult, Model model);
+
+    //LIST
+    //http://localhost:8080/product/list
+    String getAllDataList(Model model);
+
+
+    //FIND
+    //http://localhost:8080/product/find/1
+    String getFindList(Long id, Model model);
+
+    //DELETE
+    String getDelete( Long id, Model model);
 }
--- src/main/java/com/hamitmizrak/springboot/controller/mvc/ProductController.java
+++ src/main/java/com/hamitmizrak/springboot/controller/mvc/ProductController.java
@@ -5,20 +5,26 @@ import com.hamitmizrak.springboot.business.dto.ProductDto;
 import com.hamitmizrak.springboot.data.entity.ProductEntity;
 import com.hamitmizrak.springboot.data.repository.IProductRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 //Lombok
 @RequiredArgsConstructor
+@Log4j2
 
+//controller
 @Controller
 @RequestMapping("product")
+
+
 public class ProductController implements IProduct{
 
     //Injection
@@ -37,10 +43,55 @@ public class ProductController implements IProduct{
     //http://localhost:8080/product/create
     @PostMapping("create")
     @Override
-    public String  createPost(@Valid @ModelAttribute("product_create") ProductDto productDto, Model model){
+    @Transactional//Veri güvenliğini ve veri tutarlılığı için
+    public String  createPost(@Valid @ModelAttribute("product_create") ProductDto productDto, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            log.error(bindingResult.hasErrors());
+            return "product_create";
+        }
         ProductEntity productEntity= modelMapperBean.modelMapperMethod().map(productDto, ProductEntity.class);
         iProductRepository.save(productEntity);
+        return "redirect:/product_list";
+    }
+
+    //LIST
+    //http://localhost:8080/product/list
+    @GetMapping("list")
+    @Override
+    public String getAllDataList(Model model){
+         List<ProductEntity> productEntityList= iProductRepository.findAll();
+        model.addAttribute("product_key_list",productEntityList);
         return "product_list";
     }
+
+    //FIND
+    //http://localhost:8080/product/find/1
+    @GetMapping("find/{id}")
+    @Override
+    public String getFindList(@PathVariable("id") Long id, Model model){
+        Optional<ProductEntity> findEntity= iProductRepository.findById(id);
+        if(findEntity.isPresent()){
+            model.addAttribute("product_key_find",findEntity.get());
+            return "product_detail";
+        }else{
+            model.addAttribute("product_key_find",id+" nolu veri bulunamadı");
+        }
+        return "redirect:/product_list";
+    }
+
+    //DELETE
+    @GetMapping("delete/{id}")
+    @Override
+    public String getDelete(@PathVariable("id") Long id, Model model){
+        Optional<ProductEntity> findEntity= iProductRepository.findById(id);
+        if(findEntity.isPresent()){
+            model.addAttribute("product_key_delete",findEntity.get());
+            iProductRepository.deleteById(id);
+        }else{
+            model.addAttribute("product_key_delete",id+" nolu veri bulunamadı");
+        }
+        return "product_list";
+    }
+
 
 }
